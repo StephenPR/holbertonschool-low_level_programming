773f4883a54260c067950c3932ed3884cc06a90b
StephenPR <stephenboggiano.72@gmail.com>
2023-06-20 15:37:38 +0000

    o

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ int main(void)
 	}	
 	else if (n==0)
 	{
-		printf("%d is zerp\n", n );
+		printf("%d is zero\n", n );
 	}
 	else	             
 	{
-		printf("%disnegative\n",n);
+		printf("%d is negative\n",n);
 	}
 	return (0);
 }
